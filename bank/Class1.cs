f0b60197ba7711f3b2008043b68dd42e2a08106e
Jonathan Lövgren <jonathan.lovgren@skola.sala.se>
2024-09-11 10:29:16 +0200

    Add project files.

---
+++ bank/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bank
+{
+    public class BankAccount
+    {
+        int money = 0;
+        public void Deposit(int amount)
+        {
+            money = money + amount;
+        }
+        public void Withdraw(int amount)
+        {
+            if (money >= amount)
+            {
+                money = money - amount;
+            }
+            else MessageBox.Show("no money");
+
+
+        }
+        public int GetBalance()
+        {
+
+            return money;
+
+
+        }
+    }
+}
